037a2b634ef37059202b5237b7cdf7fa5ab6a7fe
HermanFassett <hermanfassett@gmail.com>
2016-01-21 22:57:03 -0800

    Update

--- app/controllers/bookHandler.server.js
+++ app/controllers/bookHandler.server.js
@@ -1,5 +1,7 @@
 var Users = require('../models/users.js');
 var Books = require('../models/books.js');
+var request = require('request');
+var parseString = require('xml2js').parseString;
 var path = process.cwd();
 
 function BookHandler () {
@@ -10,8 +12,18 @@ function BookHandler () {
 		res.render(path + "/public/add.ejs");
 	}
 	this.addBook = function(req, res) {
-		console.log(req.query);
-		res.json(req.query);
+		var book = req.body.book;
+		var url = "https://www.goodreads.com/search/index.xml";
+		var params = "?key="+process.env.GR_KEY+"&q="+book;
+		request(url+params, function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+				parseString(body, function (err, result) {
+			    res.json(result);
+				});
+		  }
+			else res.json(error)
+		});
+		//res.json(req.body);
 	}
 }
 module.exports = BookHandler;
