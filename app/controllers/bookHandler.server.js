96a15e565de4318e393a0a2f710b3637bea3a45a
HermanFassett <hermanfassett@gmail.com>
2016-01-23 22:51:43 -0800

    Adding books

--- app/controllers/bookHandler.server.js
+++ app/controllers/bookHandler.server.js
@@ -6,24 +6,55 @@ var path = process.cwd();
 
 function BookHandler () {
 	this.getBooks = function(req, res) {
-		res.render(path + "/public/books.ejs", {books: [1,2,3]});
+		Books.find({}, function(err, data) {
+			var books = (data[0].books.length > 0) ? data[0].books : [];
+			res.render(path + "/public/books.ejs", {books: books});
+		})
 	}
 	this.addBooks = function(req, res) {
-		res.render(path + "/public/add.ejs");
+		if (!req.user) {
+			res.contentType('application/json');
+			var data = JSON.stringify('/login')
+			res.header('Content-Length', data.length);
+			return res.end(data);
+		}
+		Users.findOne({"profile.name": req.user.profile.name}, function(err, data) {
+			res.render(path + "/public/add.ejs", {books:data.books});
+		});
 	}
 	this.addBook = function(req, res) {
+		if (!req.user) {
+			res.contentType('application/json');
+			var data = JSON.stringify('/login')
+			res.header('Content-Length', data.length);
+			return res.end(data);
+		}
 		var book = req.body.book;
 		var url = "https://www.goodreads.com/search/index.xml";
 		var params = "?key="+process.env.GR_KEY+"&q="+book;
 		request(url+params, function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 				parseString(body, function (err, result) {
-			    res.json(result);
+					var data = result.GoodreadsResponse.search[0].results[0].work[0].best_book[0] ;
+					var book = {
+						title: data.title[0],
+						image_url: data.image_url[0],
+						author: data.author[0].name[0]
+					}
+					Users.findOneAndUpdate({"profile.name": req.user.profile.name}, {$push: {books:book}}, function(err, obj) {
+						if (err) console.error(err);
+						else {
+							Books.findOneAndUpdate({}, {$push: {books: book}}, {upsert: true}, function(err) {
+								Users.findOne({"profile.name": req.user.profile.name}, function(err, data) {
+									res.render(path + "/public/add.ejs", {books:data.books});
+								});
+							});
+						}
+					});
 				});
 		  }
 			else res.json(error)
 		});
-		//res.json(req.body);
 	}
 }
 module.exports = BookHandler;
